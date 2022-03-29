575f4defddcdea6109fcdc0e193c5048d29cd397
jirayuk sae-lim <jirayuk.sa@worldrewardsolutions.com>
2022-03-29 16:05:05 +0700

    frist commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React,{useState, useEffect} from 'react'
+import * as AiIcons from "react-icons/ai";
 import './App.css';
 
 function App() {
+
+  const [data,setData] = useState([]);
+  const [keyWord,setKeyWord] = useState('');
+
+  useEffect(initialValue,[]);
+
+  function initialValue(){
+    fetch('https://api.publicapis.org/categories')
+    .then(res=>res.json())
+    .then((response)=>{
+      setData(response.categories);
+    })
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p id='title'>What are you looking for?</p>
+      <div className='boxSearch'>
+        <AiIcons.AiOutlineSearch className='icon'/>
+        <input type='text' placeholder='enter your text' onChange={(e)=>{setKeyWord(e.target.value)}}/>
+      </div>
+
+      <div className='boxText'>
+        {data?
+          data.filter(word=>word.toLowerCase().includes(keyWord)).map(item=>{
+            return(
+              
+                <p>{item}</p>
+              
+            )
+          })
+        :null}
+      </div>
     </div>
   );
 }
